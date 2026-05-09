783880629da67c6dba6806c8b7c9f87932e247ff
D3zNt <nicolascps4@gmail.com>
2026-05-09 16:17:22 +0700

    lists

---
+++ src/app/api/lists/[id]/route.ts
@@ -0,0 +1,47 @@
+import { ListsService } from "@/src/app/modules/lists/lists.service";
+
+export async function GET(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    try {
+        const { id } = await params;
+
+        const service =
+            await ListsService.create();
+
+        const data =
+            await service.getListById(id);
+
+        return Response.json(data);
+
+    } catch (error: any) {
+        return Response.json(
+            { error: error.message },
+            { status: 400 }
+        );
+    }
+}
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    try {
+        const { id } = await params;
+
+        const service =
+            await ListsService.create();
+
+        const data =
+            await service.deleteList(id);
+
+        return Response.json(data);
+
+    } catch (error: any) {
+        return Response.json(
+            { error: error.message },
+            { status: 400 }
+        );
+    }
+}
---
+++ src/app/api/lists/route.ts
@@ -0,0 +1,21 @@
+import { ListsService } from "@/src/app/modules/lists/lists.service";
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+
+        const service =
+            await ListsService.create();
+
+        const data =
+            await service.createList(body);
+
+        return Response.json(data);
+
+    } catch (error: any) {
+        return Response.json(
+            { error: error.message },
+            { status: 400 }
+        );
+    }
+}
